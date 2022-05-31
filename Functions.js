08e1b44f1c42e406e3a2ea281c75cd2561765f4e
RuyBarbosa22 <ruy.castro@sptech.school>
2022-05-31 18:57:06 -0300

    Comentando código Js e mudando algumas cores, continuar a arrumar cores depois e correções textuais no quiz

--- Functions.js
+++ Functions.js
@@ -85,61 +85,76 @@ var respostasC = [
   'Madeline',
 ];
 
-function Quiz() {
-  if (contadorQuestao == 0) {
+
+// função quiz começa o jogo
+
+function Quiz() { // defino quais divs ficam a mostra para o usuário
+  if (contadorQuestao == 0) { // validação que inicia o jogo, que tira a div de apresentação e apresenta a div do quiz
     ct_btn_quiz.style.display = 'none';
     ct_score.style.display = 'none';
     ct_quiz.style.display = 'flex';
   }
 
-  if (contadorQuestao == 10) {
-    const acertosSpan = document.getElementById('acertos');
+
+// validação que verifica se o jogo acabou e reseta contadores
+
+  if (contadorQuestao == 10) { // casoo em algum momento o contador de questoes chegar ao limite
+    const acertosSpan = document.getElementById('acertos'); // declaro o span do html como uma constante 
     const errosSpan = document.getElementById('erros');
 
-    ct_quiz.style.display = 'none';
+    ct_quiz.style.display = 'none'; //defino a div do quiz como invisível para apresentar a tela de resultados
     ct_score.style.display = 'flex';
-    acertosSpan.innerHTML = `Acertos: ${acertos}`;
+    acertosSpan.innerHTML = `Acertos: ${acertos}`; //apresento o valor da quantidade de acertos (variável) para o usuário
     errosSpan.innerHTML = `Erros: ${erros}`;
-    console.log(acertos, erros);
-    contadorQuestao = 0;
-    return;
+    contadorQuestao = 0; // defino o valor das questões como 0 para caso o usuário queira recomeçar
+    erros = 0 // o mesmo para os contadores de pontuação
+    acertos = 0
+    return; //  para o programa
   }
 
-  cleanAlternativesStyle();
-  handleAlternative(contadorQuestao);
+// chamando funções para continuidade do jogo
+
+  cleanAlternativesStyle(); // função para limpar a cor das bordas das alternativas
+  handleAlternative(contadorQuestao); // função que insere as informações da matriz no HTML
 }
 
-function handleAlternative(alternative) {
-  const alternativas = document.querySelectorAll('.alternativa');
-  pergunta.innerHTML = `${alternative + 1} - ${questoes[alternative]}`;
+// valida qual opção usuário escolheu e se está certa e imprime os resultados na tela
 
-  alternativas.forEach((alternativa, index) => {
-    alternativa.innerHTML = opcoes[alternative][index];
+function handleAlternative(alternative) { //função que usa "alternative" como contador 
+  const alternativas = document.querySelectorAll('.alternativa'); // pega todos os elementos com a classe "alternativa" e coloca dentro de uma constante (array)
+  pergunta.innerHTML = `${alternative + 1} - ${questoes[alternative]}`; // insere as questões baseadas no contador definido como parametro da função
 
-    alternativa.onclick = () => {
+  alternativas.forEach((alternativa, index) => { // for each é como um for para cada elemento contido em "alternativas" 
+    alternativa.innerHTML = opcoes[alternative][index]; // o for each insere o valor do vetor com as alternativas baseado na posição de "alternative" (contador - eixo Y)
+    //  e o número da questão baseado em "index" (contador - eixo X) da matriz opções
+
+    alternativa.onclick = () => { // quando o usuário clicar em alguma das divs com a classe alternativa
       // se ta certo ou errado
-      if (opcoes[alternative][index] == respostasC[alternative]) {
-        alternativa.classList.add('correct');
+      if (opcoes[alternative][index] == respostasC[alternative]) { // valida se o valor que o usuário escolheu se assemelha com o vetor com as respostas certas
+        // baseados na posição de "alternative", que representa qual questão entamos dentro da matriz, ou em qual posição no eixo Y.
+        alternativa.classList.add('correct'); // caso estiver certo adiciona uma classe verde e soma o contador de acertos
         acertos++;
       } else {
-        alternativa.classList.add('incorrect');
+        alternativa.classList.add('incorrect'); // caso contrario adiciona uma classe vermelha e soma no contador de erros
         erros++;
       }
-      setTimeout(() => {
-        contadorQuestao++;
-        Quiz();
-      }, 600);
+      setTimeout(() => { // define um delay pra que o quiz passe para a próxima questão
+        contadorQuestao++; //soma no contador que identifica em qual questão estamos
+        Quiz(); // reinicia a função do quiz
+      }, 600); // 600ms
     };
   });
 }
 
-function cleanAlternativesStyle() {
-  const alternativas = document.querySelectorAll('.alternativa');
-  alternativas.forEach(alternativa => {
-    if (alternativa.classList.contains('correct')) {
+// função que basicamente tira as classes de estilização de certo ou errado (bordas verdes e vermelhas)
+
+function cleanAlternativesStyle() { // função que limpa as cores das alternativas selecionadas
+  const alternativas = document.querySelectorAll('.alternativa');  // pega as todas as divs de alternativas 
+  alternativas.forEach(alternativa => { // e valida uma por uma
+    if (alternativa.classList.contains('correct')) { // e caso ela tiver a classe correct ela remove a borda verde
       alternativa.classList.remove('correct');
     }
-    if (alternativa.classList.contains('incorrect')) {
+    if (alternativa.classList.contains('incorrect')) { // e o mesmo aqui, caso ela tiver a classe incorrect ela remove a borda vermelha
       alternativa.classList.remove('incorrect');
     }
   });
